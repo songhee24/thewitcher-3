8a007a85209e73ca061d4e415df6da873e2df1e4
songhee24 <44497623+songhee24@users.noreply.github.com>
2018-12-08 12:17:06 +0600

    Add files via upload

---
+++ главный проект/script.js
@@ -0,0 +1,16 @@
+var name = 'Islambek';
+var surname = 'Toichuev';
+
+console.log(`hello ${name} ${surname}`)
+
+var age = 26;
+var step = 12; 
+
+
+console.log(age + step)
+console.log(age - step)
+console.log(age * step)
+console.log(age / step)
+
+
+
